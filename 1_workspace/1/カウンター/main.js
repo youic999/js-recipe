ba227f6c0cbd78ada8d4eb091e538b8cb4d06ac5
youic <youichirou2017@gmail.com>
2021-02-03 19:39:37 +0900

    カウンター発展１

---
+++ 1_workspace/1/カウンター/main.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const doubleButton = document.getElementById("double-button")
+
+let count = 0
+
+plusButton.onclick = function(){
+  count ++
+  display.textContent= count
+}
+
+minusButton.onclick = function(){
+  count --
+  display.textContent= count
+}
+
+doubleButton.onclick = function(){
+  count *= 2
+  display.textContent= count
+}
