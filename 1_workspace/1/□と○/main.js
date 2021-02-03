7bd1748acf58c9b3fb82ca96a700392a96e119d2
youic <youichirou2017@gmail.com>
2021-02-03 20:13:48 +0900

    □と○発展１

---
+++ 1_workspace/1/□と○/main.js
@@ -0,0 +1,9 @@
+const figure = document.getElementById("figure")
+
+figure.onmouseover = function(){
+  figure.classList.toggle('rounded')
+}
+
+figure.onmouseout = function(){
+  figure.classList.toggle('rounded')
+}
