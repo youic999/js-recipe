dd14e50091acad87942375d1594c5d55fb8e8fb2
youic <youichirou2017@gmail.com>
2021-02-07 03:18:37 +0900

    myWeb

--- 1_workspace/MyWeb/main.js
+++ 1_workspace/MyWeb/main.js
@@ -1,7 +1,13 @@
 const pushLike = document.getElementById("like")
 const display = document.getElementById("display")
 const myName = document.getElementById("name")
-const motherName = document.getElementById("mother")
+const univ = document.getElementById("univ")
+const circle = document.getElementById("circle")
+const hob = document.getElementById("hob")
+const nameButton = document.getElementById("name-button")
+const univButton = document.getElementById("univ-button")
+const circleButton = document.getElementById("circle-button")
+const hobButton = document.getElementById("hob-button")
 
 let like = 0
 display.textContent = 0
@@ -9,22 +15,34 @@ display.textContent = 0
 pushLike.onclick = function(){
   like = like+1
   display.textContent = like
-  if(like===100){
-    motherName.textContent="谷淳子"
-  }
-  if(like===10){
-    myName.textContent="谷陽一朗"
-  }  
 }
 
-myName.onclick = function(){
-  if(like>10){
-    myName.textContent="谷陽一朗"
+nameButton.onclick = function(){
+  if(like>9){
+    myName.textContent="名前：谷陽一朗"
+    like -= 10
+    display.textContent = like
   }
 }
 
-motherName.onclick = function(){
-  if(like>100){
-    motherName.textContent="谷淳子"
+univButton.onclick = function(){
+  if(like>14){
+    univ.textContent="大学・学部：京都大学総合人間学部"
+    like -= 15
+    display.textContent = like
+  }
+}
+circleButton.onclick = function(){
+  if(like>14){
+    circle.textContent="サークル：アカペラ"
+    like -= 15
+    display.textContent = like
+  }
+}
+hobButton.onclick = function(){
+  if(like>19){
+    hob.textContent="趣味：ギター・読書"
+    like -= 20
+    display.textContent = like
   }
 }
